8c26cc60b1ed69e97b7f10d4a799f8ceb9c369d6
Gaabuto <khanh040604@gmail.com>
2026-03-11 13:08:14 +0700

    3

---
+++ ss3/src/btvn/bai1/Bai1.java
@@ -0,0 +1,9 @@
+package btvn.bai1;
+
+public class Bai1 {
+    public static void main(String[] args) {
+        User user1 = new User("alice", "email", "active");
+        User user2 = new User("bob", "email", "inactive");
+
+    }
+}
---
+++ ss3/src/btvn/bai1/User.java
@@ -0,0 +1,4 @@
+package btvn.bai1;
+
+public record User(String username, String email, String Status) {
+}
